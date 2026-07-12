6f0e17546284a0639adc2e966dba890670c09719
Ashutosh Dash <dashashutosh1999@gmail.com>
2026-07-12 20:39:53 +0530

    chore: remove unused OpenGraph and Twitter image components

--- app/twitter-image.tsx
+++
@@ -1,8 +0,0 @@
-import { OG_IMAGE_SIZE, renderOgImage } from "@/lib/og-image"
-
-export const size = OG_IMAGE_SIZE
-export const contentType = "image/png"
-
-export default function TwitterImage() {
-  return renderOgImage()
-}
